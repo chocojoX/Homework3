0a538fd3c2dc20a86b80f25244240a089f54dd67
chocojoX <joseph.budin@polytechnique.edu>
2017-11-11 21:02:27 +0100

    Reorganized all tests

--- main_script.py
+++ main_script.py
@@ -19,25 +19,31 @@ if __name__=='__main__':
 
     # Testing one newton step
     x_new, gap = dampedNewtonStep(x, f, g, h)
-    print( "x=",x_new, "gap=%.3f" %gap, "Phi_t(x)=%.2f" %f(x_new), "Phi_10000=", phi(x_new, 10000, Q, p, A, b)/10000)
+    # print( "x=",x_new, "gap=%.3f" %gap, "Phi_t(x)=%.2f" %f(x_new), "Phi_10000=", phi(x_new, 10000, Q, p, A, b)/10000)
 
     # Testing damped Newton
     x_star, hist = dampedNewton(x,f,g,h,tol)
-    print(x_star, f(x_star), phi(x_star, 10000, Q, p, A, b)/10000)
+    # print(x_star, f(x_star), phi(x_star, 10000, Q, p, A, b)/10000)
 
     # Testing Newton backtracking line-search
     x_star, hist = newtonLS(x,f,g,h,tol)
-    print(x_star, f(x_star), phi(x_star, 10000, Q, p, A, b)/10000)
+    # print(x_star, f(x_star), phi(x_star, 10000, Q, p, A, b)/10000)
 
     # Testing the barrier method
     mu = 2
-    x_star, hist = barr_method(Q, p, A, b, x, mu, tol)
-    print(x_star, t, f(x_star), phi(x_star, 1000, Q, p, A, b)/1000)
+    x_star, hist, duality_gaps = barr_method(Q, p, A, b, x, mu, tol)
+    # print(x_star, t, f(x_star), phi(x_star, 1000, Q, p, A, b)/1000)
 
     # Testing the SVM on the Iris data
     X, Y = import_data()
+
+    duality_gap(X, Y)
+
+
+    cross_validate(X, Y)
+
     X_train, Y_train, X_test, Y_test = split_data(X, Y, 0.8)
-    w, z = solve_SVM(X_train, Y_train, tau=0.02, mu=5, tol=0.01)
+    w, z = solve_SVM(X_train, Y_train, tau=0.001, mu=5, tol=0.01)
     pred, perf = test_SVM(X_train, Y_train, w)
     print("Performance of classifier on training data : %.1f %% accuracy" %(100*perf))
     pred, perf = test_SVM(X_test, Y_test, w)
